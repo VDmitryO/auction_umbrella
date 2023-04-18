b194ba5153f294335c658991b805807b2c9f6be6
VDmitryO <vshk97@mail.ru>
2023-04-18 12:29:10 +0300

    add user logic

--- apps/auction/mix.exs
+++ apps/auction/mix.exs
@@ -29,7 +29,9 @@ defmodule Auction.MixProject do
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.9.2"},
       {:postgrex, "~> 0.16.5"},
-      {:uuid, "~> 1.1.8"}
+      {:uuid, "~> 1.1.8"},
+      {:comeonin, "~> 4.1"},
+      {:pbkdf2_elixir, "~> 0.12"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
