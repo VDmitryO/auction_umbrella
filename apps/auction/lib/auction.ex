746619f8fb485339f2f505ef5fbaf154cc9f4ac5
VDmitryO <vshk97@mail.ru>
2023-07-21 17:02:00 +0300

    add bids

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -1,5 +1,5 @@
 defmodule Auction do
-  alias Auction.{Item, User, Password}
+  alias Auction.{Bid, Item, User, Password}
 
   @repo Auction.Repo
 
@@ -53,5 +53,11 @@ defmodule Auction do
     else
       _ -> Password.dummy_verify()
     end
+  end
+
+  def insert_bid(params) do
+    %Bid{}
+    |> Bid.changeset(params)
+    |> @repo.insert()
   end
 end
